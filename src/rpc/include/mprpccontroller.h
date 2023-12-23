9cfb60c3f6d4e636ffdd6a76b26d0ec24ec549e0
swx <cwl990110@163.com>
2023-12-23 19:20:17 +0800

    feat:rpc finsh

---
+++ src/rpc/include/mprpccontroller.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <google/protobuf/service.h>
+#include <string>
+
+class MprpcController : public google::protobuf::RpcController
+{
+public:
+    MprpcController();
+    void Reset();
+    bool Failed() const;
+    std::string ErrorText() const;
+    void SetFailed(const std::string& reason);
+
+    // 目前未实现具体的功能
+    void StartCancel();
+    bool IsCanceled() const;
+    void NotifyOnCancel(google::protobuf::Closure* callback);
+private:
+    bool m_failed; // RPC方法执行过程中的状态
+    std::string m_errText; // RPC方法执行过程中的错误信息
+};
