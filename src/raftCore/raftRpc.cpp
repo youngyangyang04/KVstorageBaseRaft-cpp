66c22090381611d532b3d6a45ea47a4772c1d7a4
swx <cwl990110@163.com>
2023-12-31 13:46:19 +0800

    fix:code format

--- src/raftCore/raftRpc.cpp
+++ src/raftCore/raftRpc.cpp
@@ -8,37 +8,33 @@
 #include <mprpccontroller.h>
 
 
-bool RaftRpc::AppendEntries(raftRpcProctoc::AppendEntriesArgs *args, raftRpcProctoc::AppendEntriesReply *response)
- {
+bool RaftRpc::AppendEntries(raftRpcProctoc::AppendEntriesArgs *args, raftRpcProctoc::AppendEntriesReply *response) {
+    MprpcController controller;
+    stub_->AppendEntries(&controller, args, response, nullptr);
+    return !controller.Failed();
+}
 
-     MprpcController controller;
-     stub_->AppendEntries(&controller, args, response, nullptr);
-     return !controller.Failed();
- }
+bool RaftRpc::InstallSnapshot(raftRpcProctoc::InstallSnapshotRequest *args,
+                              raftRpcProctoc::InstallSnapshotResponse *response) {
+    MprpcController controller;
+    stub_->InstallSnapshot(&controller, args, response, nullptr);
+    return !controller.Failed();
+}
 
-bool RaftRpc::InstallSnapshot(raftRpcProctoc::InstallSnapshotRequest *args, raftRpcProctoc::InstallSnapshotResponse *response)
- {
-     MprpcController controller;
-     stub_->InstallSnapshot(&controller, args, response, nullptr);
-     return !controller.Failed();
- }
+bool RaftRpc::RequestVote(raftRpcProctoc::RequestVoteArgs *args, raftRpcProctoc::RequestVoteReply *response) {
+    MprpcController controller;
+    stub_->RequestVote(&controller, args, response, nullptr);
+    return !controller.Failed();
+}
 
-bool RaftRpc::RequestVote(raftRpcProctoc::RequestVoteArgs *args, raftRpcProctoc::RequestVoteReply *response)
- {
-     MprpcController controller;
-     stub_->RequestVote(&controller, args, response, nullptr);
-     return !controller.Failed();
- }
 //先开启服务器，再尝试连接其他的节点，中间给一个间隔时间，等待其他的rpc服务器节点启动
 
-RaftRpc::RaftRpc(std::string ip,short port)
- {
-     //*********************************************  */
-     //发送rpc设置
-     stub_ = new raftRpcProctoc::raftRpc_Stub(new MprpcChannel(ip, port,true));
- }
+RaftRpc::RaftRpc(std::string ip, short port) {
+    //*********************************************  */
+    //发送rpc设置
+    stub_ = new raftRpcProctoc::raftRpc_Stub(new MprpcChannel(ip, port, true));
+}
 
-RaftRpc::~RaftRpc()
- {
-     delete stub_;
- }
+RaftRpc::~RaftRpc() {
+    delete stub_;
+}
