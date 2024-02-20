ebef34e775088cd3ab78c6b0d252d8ef7f800137
siwuxie <example@gmail.com>
2024-02-20 22:01:27 +0800

    add format.sh and format codes
    1.添加了format.sh ，因为发现在cmake文件中直接写命令会存在\反邪杆转义的问题，直接写在sh文件中了
    2.在cmake中添加了相关的命令

--- src/raftClerk/raftServerRpcUtil.cpp
+++ src/raftClerk/raftServerRpcUtil.cpp
@@ -3,34 +3,29 @@
 //
 #include "raftServerRpcUtil.h"
 
-
-
-//kvserver不同于raft节点之间，kvserver的rpc是用于clerk向kvserver调用，不会被调用，因此只用写caller功能，不用写callee功能
+// kvserver不同于raft节点之间，kvserver的rpc是用于clerk向kvserver调用，不会被调用，因此只用写caller功能，不用写callee功能
 //先开启服务器，再尝试连接其他的节点，中间给一个间隔时间，等待其他的rpc服务器节点启动
-raftServerRpcUtil::raftServerRpcUtil(std::string ip,short port)
-{
-    //*********************************************  */
-    // 接收rpc设置
-    //*********************************************  */
-    //发送rpc设置
-    stub = new raftKVRpcProctoc::kvServerRpc_Stub(new MprpcChannel(ip, port,false));
+raftServerRpcUtil::raftServerRpcUtil(std::string ip, short port) {
+  //*********************************************  */
+  // 接收rpc设置
+  //*********************************************  */
+  //发送rpc设置
+  stub = new raftKVRpcProctoc::kvServerRpc_Stub(new MprpcChannel(ip, port, false));
 }
 
-raftServerRpcUtil::~raftServerRpcUtil() {
-    delete stub;
-}
+raftServerRpcUtil::~raftServerRpcUtil() { delete stub; }
 
 bool raftServerRpcUtil::Get(raftKVRpcProctoc::GetArgs *GetArgs, raftKVRpcProctoc::GetReply *reply) {
-    MprpcController controller;
-    stub->Get(&controller, GetArgs, reply, nullptr);
-    return !controller.Failed();
+  MprpcController controller;
+  stub->Get(&controller, GetArgs, reply, nullptr);
+  return !controller.Failed();
 }
 
 bool raftServerRpcUtil::PutAppend(raftKVRpcProctoc::PutAppendArgs *args, raftKVRpcProctoc::PutAppendReply *reply) {
-    MprpcController controller;
-    stub->PutAppend(&controller, args, reply, nullptr);
-    if(controller.Failed()){
-        std::cout<<controller.ErrorText()<<endl;
-    }
-    return !controller.Failed();
+  MprpcController controller;
+  stub->PutAppend(&controller, args, reply, nullptr);
+  if (controller.Failed()) {
+    std::cout << controller.ErrorText() << endl;
+  }
+  return !controller.Failed();
 }
--- src/raftCore/raftRpcUtil.cpp
+++ src/raftCore/raftRpcUtil.cpp
@@ -7,34 +7,31 @@
 #include <mprpcchannel.h>
 #include <mprpccontroller.h>
 
-
 bool RaftRpcUtil::AppendEntries(raftRpcProctoc::AppendEntriesArgs *args, raftRpcProctoc::AppendEntriesReply *response) {
-    MprpcController controller;
-    stub_->AppendEntries(&controller, args, response, nullptr);
-    return !controller.Failed();
+  MprpcController controller;
+  stub_->AppendEntries(&controller, args, response, nullptr);
+  return !controller.Failed();
 }
 
 bool RaftRpcUtil::InstallSnapshot(raftRpcProctoc::InstallSnapshotRequest *args,
-                              raftRpcProctoc::InstallSnapshotResponse *response) {
-    MprpcController controller;
-    stub_->InstallSnapshot(&controller, args, response, nullptr);
-    return !controller.Failed();
+                                  raftRpcProctoc::InstallSnapshotResponse *response) {
+  MprpcController controller;
+  stub_->InstallSnapshot(&controller, args, response, nullptr);
+  return !controller.Failed();
 }
 
 bool RaftRpcUtil::RequestVote(raftRpcProctoc::RequestVoteArgs *args, raftRpcProctoc::RequestVoteReply *response) {
-    MprpcController controller;
-    stub_->RequestVote(&controller, args, response, nullptr);
-    return !controller.Failed();
+  MprpcController controller;
+  stub_->RequestVote(&controller, args, response, nullptr);
+  return !controller.Failed();
 }
 
 //先开启服务器，再尝试连接其他的节点，中间给一个间隔时间，等待其他的rpc服务器节点启动
 
 RaftRpcUtil::RaftRpcUtil(std::string ip, short port) {
-    //*********************************************  */
-    //发送rpc设置
-    stub_ = new raftRpcProctoc::raftRpc_Stub(new MprpcChannel(ip, port, true));
+  //*********************************************  */
+  //发送rpc设置
+  stub_ = new raftRpcProctoc::raftRpc_Stub(new MprpcChannel(ip, port, true));
 }
 
-RaftRpcUtil::~RaftRpcUtil() {
-    delete stub_;
-}
+RaftRpcUtil::~RaftRpcUtil() { delete stub_; }
